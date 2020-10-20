674b20ed8c3d0bda857353af3802577d67b87e6c
GorelovaP <70329025+GorelovaP@users.noreply.github.com>
2020-10-20 13:09:11 +0300

    add: new code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+let str = String(n);
+let newStr= '';
+
+for(var i = str.length-1; i>=0; i--){
+newStr += str[i];
+}
+
+if (newStr[newStr.length - 1]==='-')
+{
+return newStr.slice(0, newStr.length - 1 );
+}
+return newStr;
 }
